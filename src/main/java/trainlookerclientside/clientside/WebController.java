f6fd4153da2ae1beb7a4edb46f9bf2acb138f69b
ZMizgalski <zbigniew.mizgalskifullstack@gmail.com>
2021-06-07 21:30:43 +0200

    working motor control still video to fix

--- src/main/java/trainlookerclientside/clientside/WebController.java
+++ src/main/java/trainlookerclientside/clientside/WebController.java
@@ -1,9 +1,11 @@
 package trainlookerclientside.clientside;
 
+import com.diozero.api.RuntimeIOException;
+import com.diozero.devices.PCA9685;
+import com.diozero.devices.PwmLed;
+import com.diozero.internal.spi.PwmOutputDeviceFactoryInterface;
+import com.diozero.util.SleepUtil;
 import com.github.sarxos.webcam.Webcam;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.Pin;
-import com.pi4j.io.gpio.RaspiPin;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
@@ -23,8 +25,6 @@ import java.io.InputStream;
 @CrossOrigin(value = "*", maxAge = 3600)
 public class WebController {
 
-    private final Pin PIN = RaspiPin.GPIO_01;
-    public GpioPinDigitalOutput pin;
     @LocalServerPort
     private int port;
 
@@ -54,21 +54,38 @@ public class WebController {
 
     @RequestMapping(value = "/openLevelCrossing/{id}")
     public ResponseEntity<?> openLevelCrossing(@PathVariable String id) {
-//        if (pin == null) {
-//            GpioController gpioController = GpioFactory.getInstance();
-//            pin = gpioController.provisionDigitalOutputPin(PIN, "Open", PinState.HIGH);
-//        }
-//        pin.high();
+        float delay = 0.5f;
+        try (PwmOutputDeviceFactoryInterface df = new PCA9685(400);
+             PwmLed led1 = new PwmLed(df, 0);
+             PwmLed led2 = new PwmLed(df, 2);
+             PwmLed led3 = new PwmLed(df, 5)
+        ) {
+            led1.setValue(.25f);
+            led2.setValue(.25f);
+            led3.setValue(.25f);
+            SleepUtil.sleepSeconds(delay);
+        } catch (RuntimeIOException e) {
+            return ResponseEntity.badRequest().body(String.format("Something wrong with motors when opening levelCrossing with id: %s", id));
+
+        }
         return ResponseEntity.ok().body(String.format("Level crossing opened with id: %s", id));
     }
 
     @RequestMapping(value = "/closeLevelCrossing/{id}")
     public ResponseEntity<?> closeLevelCrossing(@PathVariable String id) {
-//        if (pin == null) {
-//            GpioController gpioController = GpioFactory.getInstance();
-//            pin = gpioController.provisionDigitalOutputPin(PIN, "Close", PinState.LOW);
-//        }
-//        pin.low();
+        float delay = 0.5f;
+        try (PwmOutputDeviceFactoryInterface df = new PCA9685(400);
+             PwmLed led1 = new PwmLed(df, 0);
+             PwmLed led2 = new PwmLed(df, 2);
+             PwmLed led3 = new PwmLed(df, 5)
+        ) {
+            led1.setValue(.5f);
+            led2.setValue(.5f);
+            led3.setValue(.5f);
+            SleepUtil.sleepSeconds(delay);
+        } catch (RuntimeIOException e) {
+            return ResponseEntity.badRequest().body(String.format("Something wrong with motors when closing levelCrossing with id: %s", id));
+        }
         return ResponseEntity.ok().body(String.format("Level crossing closed with id: %s", id));
     }
 }
