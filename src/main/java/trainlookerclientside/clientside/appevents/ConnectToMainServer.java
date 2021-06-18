c4f0520bc3229867b379470a1b641dc79727bc0e
ZMizgalski <zbigniew.mizgalskifullstack@gmail.com>
2021-06-19 01:18:41 +0200

    Socket update

--- src/main/java/trainlookerclientside/clientside/appevents/ConnectToMainServer.java
+++ src/main/java/trainlookerclientside/clientside/appevents/ConnectToMainServer.java
@@ -12,6 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.UUID;
 
@@ -22,6 +26,9 @@ public class ConnectToMainServer implements ApplicationListener<ApplicationReady
     @Value("${server.port}")
     private int localPort;
 
+    @Value("${main-server-socket.port}")
+    private int socketPort;
+
     @Value("${main-server.ip}")
     private String ip;
 
@@ -31,16 +38,24 @@ public class ConnectToMainServer implements ApplicationListener<ApplicationReady
     @Override
     public void onApplicationEvent(@NotNull ApplicationReadyEvent event) {
         try {
-            Socket s = new Socket(ip, mainPort);
-            RestTemplate restTemplate = new RestTemplate();
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            HttpEntity<String> request = new HttpEntity<>("{\"levelCrossingIP\":\"" + "http://" + s.getLocalAddress().getHostAddress() + ":" + localPort + "\", " + "\"id\":\"" + UUID.randomUUID().toString() + "\"}", headers);
-            ResponseEntity<String> response = restTemplate.postForEntity(
-                    "http://" + ip + ":" + mainPort + "/api/server/registerNewLevelCrossing",
-                    request,
-                    String.class);
-            log.warn(response.getBody());
+            Socket s = new Socket(ip, socketPort);
+            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+            out.write("http://" + s.getLocalAddress().getHostAddress() + ":" + localPort);
+            out.close();
+
+//            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+//            dout.writeUTF("http://" + s.getLocalAddress().getHostAddress() + ":" + localPort);
+//            dout.flush();
+//            dout.close();
+//            RestTemplate restTemplate = new RestTemplate();
+//            HttpHeaders headers = new HttpHeaders();
+//            headers.setContentType(MediaType.APPLICATION_JSON);
+//            HttpEntity<String> request = new HttpEntity<>("{\"levelCrossingIP\":\"" + "http://" + s.getLocalAddress().getHostAddress() + ":" + localPort + "\", " + "\"id\":\"" + UUID.randomUUID().toString() + "\"}", headers);
+//            ResponseEntity<String> response = restTemplate.postForEntity(
+//                    "http://" + ip + ":" + mainPort + "/api/server/registerNewLevelCrossing",
+//                    request,
+//                    String.class);
+//            log.warn(response.getBody());
         } catch (Exception e) {
             log.error("Can't connect to main server:");
             log.error("=> check if main server is online");
