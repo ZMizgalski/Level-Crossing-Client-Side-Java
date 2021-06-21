be0cc0c1d3279f445082f41aaa9ead509f20ad9d
ZMizgalski <zbigniew.mizgalskifullstack@gmail.com>
2021-06-21 14:13:54 +0200

    Socket working but may need some fixes

--- src/main/java/trainlookerclientside/clientside/appevents/ConnectToMainServer.java
+++ src/main/java/trainlookerclientside/clientside/appevents/ConnectToMainServer.java
@@ -1,79 +1,48 @@
 package trainlookerclientside.clientside.appevents;
 
+import lombok.Getter;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+import si.trina.socket.live.SocketConnection;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Slf4j
 @Component
 public class ConnectToMainServer implements ApplicationListener<ApplicationReadyEvent> {
 
+    @Getter
+    private final ExecutorService pool = Executors.newFixedThreadPool(10);
     @Value("${server.port}")
     private int localPort;
-
     @Value("${main-server-socket.port}")
     private int socketPort;
-
     @Value("${main-server.ip}")
     private String ip;
-
     @Value("${main-server.port}")
     private int mainPort;
+    private boolean ipSent = false;
 
-    private boolean sendCurrentIpAddress() {
-        Socket s;
-        try {
-            s = new Socket(ip, socketPort);
-        } catch (IOException e) {
-            return false;
-        }
-        PrintWriter out;
-        try {
-            out = new PrintWriter(s.getOutputStream(), true);
-        } catch (IOException e) {
-            return false;
-        }
-        out.write("http://" + s.getLocalAddress().getHostAddress() + ":" + localPort);
-        out.close();
-        log.info("Connection has been established");
-        return true;
-    }
-
+    @SneakyThrows
     @Override
     public void onApplicationEvent(@NotNull ApplicationReadyEvent event) {
         log.info("<=====================>");
         log.info("Waiting for remote host...");
         log.info("<=====================>");
-        sendCurrentIpAddress();
-        TimerTask task = new TimerTask() {
-            public void run() {
-                Socket s;
-                try {
-                    s = new Socket(ip, socketPort);
-                } catch (IOException e) {
-                    log.warn("Trying to connect to server....");
-                    sendCurrentIpAddress();
-                    return;
-                }
-                PrintWriter out;
-                try {
-                    out = new PrintWriter(s.getOutputStream(), true);
-                } catch (IOException ignored) {
-                    return;
-                }
-                out.write("");
-                out.close();
-            }
-        };
-        new Timer().schedule(task, 1000, 1000);
+        SocketInitialized socketInitialized = new SocketInitialized("Camera socket", ip, socketPort, localPort);
+        new Thread(socketInitialized).start();
+
+//        Future<SocketConnection> socketInitializedFuture = pool.submit(socketInitialized);
+//        Socket socket = socketInitializedFuture.get().socket;
+//        System.out.println(socket);
     }
 }
